c0b7154eee78519e28b83deb732b0ecfbb95b5b3
joerow <joerow@gmail.com>
2023-01-01 17:47:23 +0800

    add CSS and image loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("Setup working as expected");
+import "./style.css";
+
+function component() {
+  const element = document.createElement("h1");
+
+  // Lodash, now imported by this script
+  element.innerHTML = "TODOODIN";
+  element.classList.add("hello");
+  return element;
+}
+
+document.getElementById("content").appendChild(component());
