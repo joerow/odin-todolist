009620f0d1e2c9e39d01afc6167b290cd6ebe1e5
joerow <joerow@gmail.com>
2023-01-02 17:30:21 +0800

    started to add content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 console.log("Setup working as expected");
 import "./style.css";
 
-function component() {
-  const element = document.createElement("h1");
+function base() {
+  const element = document.createElement("div");
+  element.classList.add("content");
 
-  // Lodash, now imported by this script
-  element.innerHTML = "TODOODIN";
-  element.classList.add("hello");
+  const topBar = document.createElement("div");
+  topBar.classList.add("topBar");
+  element.append(topBar);
+  topBar.innerHTML = "this is the top bar";
+
   return element;
 }
-
-document.body.appendChild(component());
+document.body.appendChild(base());
