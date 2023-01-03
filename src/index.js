8536d51743efde5ea99b99e126cc4288a631364b
joerow <joerow@gmail.com>
2023-01-03 11:26:53 +0800

    add grid layout

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import "./style.css";
 
 function base() {
   const element = document.createElement("div");
-  element.classList.add("content");
+  element.classList.add("grid-container");
 
-  const topBar = document.createElement("div");
-  topBar.classList.add("topBar");
-  element.append(topBar);
-  topBar.innerHTML = "this is the top bar";
+  const header = document.createElement("div");
+  header.classList.add("item1");
+  element.append(header);
+  header.innerHTML = "header";
+
+  const menu = document.createElement("div");
+  menu.classList.add("item2");
+  element.append(menu);
+  menu.innerHTML = "menu";
+
+  const main = document.createElement("div");
+  main.classList.add("item3");
+  element.append(main);
+  main.innerHTML = "main";
+
+  const right = document.createElement("div");
+  right.classList.add("item4");
+  element.append(right);
+  right.innerHTML = "right";
+
+  const footer = document.createElement("div");
+  footer.classList.add("item5");
+  element.append(footer);
+  footer.innerHTML = "footer";
 
   return element;
 }
