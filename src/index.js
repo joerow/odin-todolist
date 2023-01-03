4c5834730667572d15573141230f0ab2b4e866a0
joerow <joerow@gmail.com>
2023-01-03 19:38:43 +0800

    add new lists script

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
-console.log("Setup working as expected");
 import "./style.css";
+import Lists from "./lists.js";
+
+function header() {
+  const header = document.createElement("div");
+  header.classList.add("item1");
+  header.innerHTML = '<span class="material-symbols-outlined">menu</span>';
+  header.innerHTML +=
+    '<span class="material-symbols-outlined">account_circle</span>';
+  return header;
+}
+
+function lists() {
+  const lists = document.createElement("div");
+  lists.classList.add("item2");
+  Lists();
+  lists.innerHTML = "Lists not loaded";
+  return lists;
+}
 
 function base() {
+  // create the grid contianer
   const element = document.createElement("div");
   element.classList.add("grid-container");
   element.innerHTML =
     '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />';
 
-  const header = document.createElement("div");
-  header.classList.add("item1");
-  element.append(header);
-  header.innerHTML = '<span class="material-symbols-outlined">menu</span>';
-  const menu = document.createElement("div");
-  menu.classList.add("item2");
-  element.append(menu);
-  menu.innerHTML = "menu";
+  // create the header
+  element.append(header());
+  // create the menu
+  element.append(lists());
 
+  // create the main area
   const main = document.createElement("div");
   main.classList.add("item3");
   element.append(main);
-  main.innerHTML = "main";
+  main.innerHTML = "List items not loaded";
 
+  // create the right menu
   const right = document.createElement("div");
   right.classList.add("item4");
   element.append(right);
-  right.innerHTML = "right";
+  right.innerHTML = "Item Edit";
 
+  // create the footer
   const footer = document.createElement("div");
   footer.classList.add("item5");
   element.append(footer);
---
+++ src/lists.js
@@ -0,0 +1,5 @@
+export default function Lists() {
+  function load() {
+    console.log("load lists run");
+  }
+}
